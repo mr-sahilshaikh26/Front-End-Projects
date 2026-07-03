dc6353f7936a613bf9a197d8af68946d57d65104
Shaikh Sahil <sahilshaikh30249@gmail.com>
2026-07-03 16:50:15 +0530

    java tasks

---
+++ project19/javascript.js
@@ -0,0 +1,297 @@
+//TASK 1//
+// // let name = "SHAIKH SAHIL";
+// // console.log(`Hello, my name is ${name}.`);
+
+// // let age = "20";
+// // console.log(`I am 20 years old ${age}.`);
+
+// // let address = "Chikhli";
+// // console.log(`I live in ${address}.`);
+
+// // let Student = "true";
+// // console.log(`student :${true}.`);
+
+
+
+//TASK 2//
+// //  let number1 = 20;
+// // let number2 = 6;
+// // console.log("Addition:",number1+number2);
+// //  console.log("Substration:",number1-number2);
+// //  console.log("Multiplication:",number1*number2);
+// // console.log("Division:",number1/number2);
+// //  console.log("Reminder:",number1%number2);
+
+
+
+
+//TASK 3//
+// // let number = "150 number";
+// // console.log(number);
+// // let boolen = "true boolean";
+// // console.log( boolen);
+// // let string = "150 String ";
+// // console.log( string);
+
+
+
+
+//TASK 4//
+// // let age = 198;
+// // if (age <= 13) {
+// //     console.log("Child");
+// // } else if (age <= 17) {
+// //     console.log("Teenager");
+// // } else if (age <= 60) {
+// //     console.log("Adult");
+// // }
+// // else {
+// //     console.log("Senior Citizen");
+// // }
+
+
+
+//TASK 5//
+// // let number = 6;
+// // if (number %2===0)
+// // { console.log( number+ " :is Even number")
+// // } else{
+// //     console.log(number+" :is Odd number ")
+// // }
+
+
+//TASK 6//
+// //   let number = -1;
+// //   if (number>0){
+// //     console.log("Positive");
+// //   }
+// //   else if(number<0){
+// //     console.log("Negative");
+// //   }
+// //  else 
+// //  {
+// //     console.log("Zero");
+// //   }
+
+
+//TASK 7//
+// // let a = 90;
+// // b = 90;
+// // if(a<b)
+// //     {
+// //        console.log("Greater") ;
+// //     }
+
+// // // // else if(a>b){
+// // // //     console.log("Greater") ;
+// // // }
+// // else 
+// // {
+// //     console.log("Both number are Equal") 
+
+// // }
+
+
+//TASK 8//
+// // let number = 1;
+// // if ( number>90
+// // )
+// // {
+// //     console.log("A");
+// // }
+// // else if ( number>80){
+// //     console.log("B");
+// // }
+// // else if (number>70)
+// // {console.log("C")}
+
+// // else if (number>60)
+// // {console.log("D")}
+
+// // else
+// //     console.log("FAIL")
+
+
+
+//TASK 9//
+// // let Username = "sahil";
+// // let password = 12345;
+
+// // if (Username != "sahil") {
+// //     console.log("Userrname is not valid");
+
+// // } else if(password!=12345){
+// //     console.log("Password is not valid");
+// // } else{
+// //     console.log("Login Successful");
+// // }
+
+//TASK 10//
+// // let number = 8;
+// // if(number===1){
+// //     console.log("Monday");
+// // }
+// //  else if(number===2){
+// //     console.log("Tuesday");
+// // }
+// // else if(number===3){
+// //     console.log("Wednesday");
+// // }
+// //  else if(number===4){
+// //     console.log("Thursday");
+// // }
+// //  else if(number===5){
+// //     console.log("Friday");
+// // }
+// //  else if(number===6){
+// //     console.log("Saturday");
+// // }
+// //  else if (number===7)
+// //  {
+// //     console.log("Sunday");
+// // }
+// // else 
+// // {
+// // console.log("Invaild Day")
+// // }
+
+//TASK 11//
+// // let singal = "Blue";
+// // if( singal==="Red"){
+// //     console.log("Stop")
+// // }
+// // else if (singal==="Orange")
+// // {
+// //     console.log("Get Ready")
+// // }
+// // else if (singal==="Green"){
+// //     console.log("Go...")
+// // }
+// // else{
+// //     console.log("Invaild singal")
+// // }
+
+
+//TASK 12//
+// // let MonthNumber = 15;
+// // if(MonthNumber===1){
+// //     console.log("January")
+// //  }
+// //  else if(MonthNumber===2){
+// //     console.log("February")
+// //  }
+// //   else if(MonthNumber===3){
+// //     console.log("March")
+// //  }
+// //  else if(MonthNumber===4){
+// //     console.log("April")
+// //  }
+// //  else if(MonthNumber===5){
+// //     console.log("May")
+// //  }
+// //  else if(MonthNumber===6){
+// //     console.log("June")
+// //  }
+// //  else if(MonthNumber===7){
+// //     console.log("July")
+// //  }
+// //  else if(MonthNumber===8){
+// //     console.log("Aujust")
+// //  }
+// //  else if(MonthNumber===9){
+// //     console.log("Septemer")
+// //  }
+// //  else if(MonthNumber===10){
+// //     console.log("Octuber")
+// //  }
+// //  else if(MonthNumber===11){
+// //     console.log("November")
+// //  }
+// //   else if(MonthNumber===12){
+// //     console.log("December")
+// //  }
+// //  else{
+// //     console.log("Invalid Month")
+// //  }
+
+
+
+// BONUS TASK //
+// // let Age = 8;
+// // let Nationality = "Indian";
+
+// // if (Age >= 18 && Nationality === "Indian") {
+// //     console.log("Eligiable to Vote");
+
+// // // } else if(Nationality!="Indian"){
+// // //     console.log("Eligible to Vote");
+// // } else{
+// //     console.log("Not Eligiable");
+// // }
+
+
+// // let studentName = "Sahil";
+// // let studentAge = 18;
+// // let studentMarks = 50;
+// // let course = "Science";
+
+// // if (studentAge >= 17 && studentMarks >= 60) {
+// //     console.log("Admission Approved");
+
+// //     if (course === "Science") {
+// //         console.log("Selected Course: Science");
+// //     } else if (course === "Commerce") {
+// //         console.log("Selected Course: Commerce");
+// //     } else if (course === "Arts") {
+// //         console.log("Selected Course: Arts");
+// //     } else {
+// //         console.log("Invalid Course");
+// //     }
+// // } else {
+// //     console.log("Rejection Reason: Age or Marks below requirements");
+// // }
+
+
+
+//TASK 13//
+// let BillAmount = 5000;
+// //  let discount = 0;
+// //  let finalBill = 0; 
+
+// if (BillAmount > 5000) {
+//     discount = BillAmount / 100 * 20;
+//     finalBill = BillAmount - discount;
+
+//     // console.log("originalBill =", BillAmount);
+//     // console.log("discount =", BillAmount / 100 * 20);
+//     // console.log("finalBill =", BillAmount - discount);
+
+//     // discount=1200;
+//     // finalBill=4800;
+// }
+// else if (BillAmount > 3000) {
+//     discount = BillAmount / 100 * 15;
+//     finalBill = BillAmount - discount;
+// }
+// else if (BillAmount > 1000) {
+//     discount = BillAmount / 100 * 10;
+//     finalBill = BillAmount - discount;
+// }
+// else {
+//     discount = 0;
+//     finalBill = BillAmount;
+// }
+
+// console.log("originalBill =", BillAmount);
+// console.log("discount =", discount);
+// console.log("finalBill =", finalBill);
+
+
+
+
+
+
+
+
+
+
